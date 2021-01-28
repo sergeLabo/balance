e20836541ae257129bc3efac5ccb068ca8f3ce3a
serge <serge@localhost>
2021-01-28 12:14:26 +0100

    gros nettoyage, apres install MSI

--- run_blender_redressement.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# -*- coding: UTF-8 -*-
-
-cd /media/data/3D/projets/balance/redressement/redressement
-
-blenderplayer redressement.blend
--- run_redressement_training.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# -*- coding: UTF-8 -*-
-
-cd /media/data/3D/projets/balance/
-
-python3 redressement_training.py
